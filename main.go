7203a688b47d5ab742ff9ded0e101d6b7112d2a4
guiisgooey <49354894+guiisgooey@users.noreply.github.com>
2022-03-04 08:39:21 -0500

    moved components to utils, created a main file for example application

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	var input string
+	fmt.Println("Type a message to be encrypted:")
+	fmt.Scanln(&input)
+
+}
--- utils/DHKey.go
+++ utils/DHKey.go
@@ -6,22 +6,27 @@ type Endpoint struct {
 	privateKey   int // - the private key known only to each person
 }
 
-func newEndpoint(publicBase, publicKey2, privateKey int) Endpoint {
-	return Endpoint{publicBase, publicKey2, privateKey}
+func newEndpoint(publicBase, publicModulo, privateKey int) Endpoint {
+	//creates a endpoint struct using an oop style function
+	return Endpoint{publicBase, publicModulo, privateKey}
 }
 
 func genPartial(end Endpoint) int {
-	//This partial key is also known as the personal public key in terms of this algorithm
+	//generate public key using private key and public parts to hand over to other party
+	//this is safe to directly hand over
 	partial := Exp(end.publicBase, end.privateKey)
 	return partial % end.publicModulo
 }
 
 func genFull(end Endpoint, partialKey int) int {
+	//generate full shared secret using the other parties' public key and our personal endpoint
+	//this should not be shared directly
 	return Exp(partialKey, end.privateKey) % end.publicModulo
 }
 
 func encrypt(end Endpoint, partialKey int, message string) string {
 	var encrypted []rune //int32
+	//encode each character to an integer, add the resultant int value of the secret to encode it
 	for i, char := range message {
 		encrypted[i] = rune(int(char) + genFull(end, partialKey))
 	}
@@ -31,20 +36,24 @@ func encrypt(end Endpoint, partialKey int, message string) string {
 
 func decrypt(end Endpoint, partialKey int, encrypted string) string {
 	var message []rune
+	//perform the opposite of what we did to encrypt, to decrypt it
 	for i, char := range encrypted {
 		message[i] = rune(int(char) - genFull(end, partialKey))
 	}
 	return string(message)
 }
 
-// fast binary operation for calculating power of integers, obviously only works to int64 max
 func Exp(x, y int) int {
+	// fast binary operation for calculating power of integers, only works to int64 max
+	// this also isn't set up for negative integers for root style expressions
 	power := 1
 	for y > 0 {
+		// if y bit is 1, you could also do y%2 != 0 here
 		if y&1 != 0 {
 			power *= x
 		}
-		y >>= 1
+		//eventually this will make y == 0
+		y >>= 1 //binary right shift -> y = y/2 once, you could also just do y /= 2
 		x *= x
 	}
 	return power
